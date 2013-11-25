6a9c20c21a57542ae2cff89bcde0c290a4c7dd4f
Ha The Tung <tunght13488@gmail.com>
2013-11-26 00:51:32 +0700

    update lang

--- include/SugarObjects/templates/basic/language/vi_vi.lang.php
+++ include/SugarObjects/templates/basic/language/vi_vi.lang.php
@@ -2,31 +2,31 @@
 /*********************************************************************************
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
  * Free Software Foundation with the addition of the following permission added
  * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
  * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
  * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with
  * this program; if not, see http://www.gnu.org/licenses or write to the Free
  * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301 USA.
- * 
+ *
  * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
  * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU Affero General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
  * SugarCRM" logo. If the display of the logo is not reasonably feasible for
@@ -36,22 +36,20 @@
 
 
 $mod_strings = array(
-'LBL_ID'=>'ID',
-'LBL_DATE_ENTERED'=>'Date Created',
-'LBL_DATE_MODIFIED'=>'Date Modified', 
-'LBL_MODIFIED'=>'Modified By',
-'LBL_MODIFIED_ID'=>'Modified By Id',
-'LBL_MODIFIED_NAME'=>'Modified By Name',
-'LBL_CREATED'=>'Created By',
-'LBL_CREATED_ID'=>'Created By Id',
-'LBL_DESCRIPTION'=>'Description',
-'LBL_DELETED'=>'Deleted',
-'LBL_NAME'=>'Name',
-'LBL_CREATED_USER'=>'Created by User',
-'LBL_MODIFIED_USER'=>'Modified by User',
-'LBL_LIST_NAME'=>'Name',
-'LBL_EDIT_BUTTON' => 'Edit',
- 'LBL_REMOVE' => 'Remove',
-
-
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Ngày tạo',
+    'LBL_DATE_MODIFIED' => 'Ngày sử',
+    'LBL_MODIFIED' => 'Sửa bởi',
+    'LBL_MODIFIED_ID' => 'Sửa bởi Id',
+    'LBL_MODIFIED_NAME' => 'Sửa bởi người dùng',
+    'LBL_CREATED' => 'Tạo bởi',
+    'LBL_CREATED_ID' => 'Tạo bởi Id',
+    'LBL_DESCRIPTION' => 'Mô tả',
+    'LBL_DELETED' => 'Đã xoá',
+    'LBL_NAME' => 'Tên',
+    'LBL_CREATED_USER' => 'Tạo bởi người dùng',
+    'LBL_MODIFIED_USER' => 'Sửa bởi người dùng',
+    'LBL_LIST_NAME' => 'Tên',
+    'LBL_EDIT_BUTTON' => 'Sửa',
+    'LBL_REMOVE' => 'Xoá',
 );
